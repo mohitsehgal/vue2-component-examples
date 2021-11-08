f1e5e2d375c0b592092fc27ac43b1b6a07ac2a14
Mohit Sehgal <sehgal.mohit29@gmail.com>
2021-11-08 11:59:53 +0530

    Component Prop type example

--- src/components/ProjectInfo.vue
+++ src/components/ProjectInfo.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="row">
-    <h1>{{title}}</h1>
-    <p><strong>Employees Allocated:</strong> {{allocatedEmployeeCount}}</p>
-    <p><strong> Start Date : </strong> {{startDate}}</p>
+    <h1>{{ title }}</h1>
+    <p><strong>Employees Allocated:</strong> {{ allocatedEmployeeCount }}</p>
+    <p><strong> Start Date : </strong> {{ startDate }}</p>
   </div>
 </template>
 <script>
 export default {
-    name:'ProjectInfo',
-    props:['title','allocatedEmployeeCount','startDate']
-}
+  name: "ProjectInfo",
+  props: { title: String, allocatedEmployeeCount: Number, startDate: String },
+};
 </script>
 <style scoped>
-
 </style>
